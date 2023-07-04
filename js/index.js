ab38553fe0628609eab16787eea34f51640be4fb
Comfort Ige <eligenius@gmail.com>
2023-07-04 14:32:37 +0100

    Add font-awesome icons

--- js/index.js
+++ js/index.js
@@ -19,13 +19,16 @@ function getFeedHtml(){
           <p class="tweet-text">${tweet.tweetText}</p>
           <div class="tweet-details">
             <span class="tweet-detail">
-            ${tweet.replies.length}
+              <i class="fa-regular fa-comment-dots"></i>
+              ${tweet.replies.length}
             </span>
             <span class="tweet-detail">
-            ${tweet.likes}
+              <i class="fa-solid fa-heart"></i>
+              ${tweet.likes}
             </span>
             <span class="tweet-detail">
-            ${tweet.retweets}
+              <i class="fa-solid fa-retweet"></i>
+              ${tweet.retweets}
             </span>
           </div>   
         </div>            
