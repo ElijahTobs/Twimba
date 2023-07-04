604ed9cda549e066098c6b6206cc2f67d7a63cee
Comfort Ige <eligenius@gmail.com>
2023-07-04 12:31:23 +0100

    Add tweet boilerplate

--- js/index.js
+++ js/index.js
@@ -1,3 +1,41 @@
 import { tweetsData } from "/js/data.js";
 
-console.log(tweetsData)
+
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
+
+tweetBtn.addEventListener('click', function(){
+  console.log(tweetInput.value)
+  tweetInput.value = ""
+})
+
+function getFeedHtml(){
+  for (let tweet of tweetsData){
+    let feedHtml = `
+    <div class="tweet">
+      <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+          <p class="handle">${tweet.handle}</p>
+          <p class="tweet-text">${tweet.tweetText}</p>
+          <div class="tweet-details">
+            <span class="tweet-detail">
+            ${tweet.replies.length}
+            </span>
+            <span class="tweet-detail">
+            ${tweet.likes}
+            </span>
+            <span class="tweet-detail">
+            ${tweet.retweets}
+            </span>
+          </div>   
+        </div>            
+      </div>
+    </div>
+    `
+    console.log(feedHtml)
+  }
+}
+
+
+getFeedHtml()
