dd1f2b81ee1a5699e72c666fab0948142537395b
Comfort Ige <eligenius@gmail.com>
2023-07-04 17:37:42 +0100

    Add handleLikeClick function and eventListener on the document object

--- js/index.js
+++ js/index.js
@@ -5,6 +5,26 @@ const tweetBtn = document.getElementById('tweet-btn')
 const feedEl = document.getElementById("feed")
 
 
+tweetBtn.addEventListener('click', function(){
+  console.log(tweetInput.value)
+})
+
+document.addEventListener('click', function(e){
+  const targetDataset = e.target.dataset
+  if (targetDataset.reply){
+    handleLikeClick(targetDataset.reply)
+  } else if (targetDataset.like){
+    handleLikeClick(targetDataset.like)    
+  } else if (targetDataset.retweet){
+    handleLikeClick(targetDataset.retweet)
+  }
+})
+
+
+function handleLikeClick(tweetId) {
+  console.log(tweetId)
+}
+
 function getFeedHtml(){
   let feedHtml = ``
 
@@ -19,15 +39,15 @@ function getFeedHtml(){
           <p class="tweet-text">${tweet.tweetText}</p>
           <div class="tweet-details">
             <span class="tweet-detail">
-              <i class="fa-regular fa-comment-dots"></i>
+              <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
               ${tweet.replies.length}
             </span>
             <span class="tweet-detail">
-              <i class="fa-solid fa-heart"></i>
+              <i class="fa-solid fa-heart" data-like="${tweet.uuid}"></i>
               ${tweet.likes}
             </span>
             <span class="tweet-detail">
-              <i class="fa-solid fa-retweet"></i>
+              <i class="fa-solid fa-retweet" data-retweet="${tweet.uuid}"></i>
               ${tweet.retweets}
             </span>
           </div>   
